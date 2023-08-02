f5b22808ba5a5684a44a0116c06b683dd74c3f55
Marcos Antônio de Almeida <marcosantoniomgy@gmail.com>
2023-08-02 11:10:56 -0300

    Meu primeiro commit

---
+++ src/main/java/br/com/marcosAlmeida/projetoAcademico/ProjetoAcademicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcosAlmeida.projetoAcademico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAcademicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcosAlmeida/projetoAcademico/ProjetoAcademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcosAlmeida.projetoAcademico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAcademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
